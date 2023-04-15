f65d445cf93074d77428dca01f53f546be7d80ab
Ivan-Kulagin <ivan.kulagin2001@gmail.com>
2023-04-15 13:04:06 +0300

    add otp components

---
+++ src/views/OTPCode.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Enter code</h1>
+    <p>
+      We've sent an SMS with an activation code to your phone
+      <strong>{{ phone }}</strong>
+    </p>
+    <div class="otp-wrapper"><v-otp-input /></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.otp-wrapper {
+  max-width: 320px;
+  margin: 0 auto;
+}
+</style>
+
+<script>
+export default {
+  name: "OTPPhone",
+  props: {
+    phone: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/views/OTPPhone.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <h1 class="heading-text">Enter a phone number</h1>
+    <div class="otp-wrapper">
+      <p class="p-text">
+        Please confirm your country code and enter your phone number
+      </p>
+    </div>
+  </v-container>
+</template>
+
+<style scoped lang="scss">
+.otp-wrapper {
+  max-width: 320px;
+  margin: 0 auto;
+}
+
+.heading-text {
+  margin-top: 48px;
+}
+
+.p-text {
+  padding-top: 16px;
+}
+</style>
+
+<script>
+export default {
+  name: "OTPPhone",
+  data() {
+    return {
+      phone: null,
+    };
+  },
+};
+</script>
